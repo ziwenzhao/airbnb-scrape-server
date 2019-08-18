cf0d1c25e50cb5fc943b7532448d05014b65e35d
ziwen zhao <ziwenzhao@MacBook-Pro-4.local>
2019-08-18 12:04:32 -0400

    prepare for aws deploy

--- application.py
+++ application.py
@@ -3,19 +3,23 @@ from flask import request
 from flask import abort
 from flask import Response
 from flask import jsonify
+print('applcaiton.py started')
 from time import sleep
 import os
 import subprocess
 import uuid
 import logging
 from exception_model import InvalidUsage
-
-app = Flask(__name__)
-
-app.logger.setLevel(logging.INFO)
+from maintain_files import clean_files_periodically
 
 
-@app.route('/scrape_homes', methods = ['post'])
+application = Flask(__name__)
+
+application.logger.setLevel(logging.INFO)
+
+clean_files_periodically(application)
+
+@application.route('/scrape_homes', methods = ['post'])
 def scrape_homes():
     
     if request.form.get('url') is None or request.form.get('max_page_number') is None:
@@ -23,36 +27,35 @@ def scrape_homes():
                            status_code=422, payload={'url': '/scrape_homes', 'method': 'post'})
     job_id = uuid.uuid1()
     try:
-        app.logger.info('start scrape homes')
+        application.logger.info('start scrape homes')
         subprocess.check_output('cd ./airbnb\ homes\ scrape && scrapy crawl home -a url="' + request.form.get('url') + '" -a max_page_number=' \
                         + request.form.get('max_page_number') + ' -t json -o -> home_' + str(job_id) + '.json', shell=True)
-        app.logger.info('scrape homes done')
+        application.logger.info('scrape homes done')
         return {'job_id': job_id}
     except Exception as e:
-        app.logger.error('scrape homes error: ' + str(e))
+        application.logger.error('scrape homes error: ' + str(e))
         raise InvalidUsage(message=str(e), status_code=500)
 
 
 
-
-@app.route('/get_homes', methods=['get'])
+@application.route('/get_homes', methods=['get'])
 def get_homes():
     job_id = request.args.get('job_id') 
     if job_id is None:
         raise InvalidUsage(message='request missed query parameters. The paramters of job_id is necessary',\
                            status_code=422, payload={'url': '/get_homes', 'method':'get'})
     try:
-        app.logger.info('open file: ./airbnb homes scrape/home_' + job_id + '.json')
+        application.logger.info('open file: ./airbnb homes scrape/home_' + job_id + '.json')
         with open('./airbnb homes scrape/home_' + job_id + '.json' ) as file:
             return file.read()
     except Exception as e:
-        app.logger.error('file: ./airbnb homes scrape/home_' + job_id + '.json open failed: ' + str(e))
+        application.logger.error('file: ./airbnb homes scrape/home_' + job_id + '.json open failed: ' + str(e))
         raise InvalidUsage(message=str(e), status_code=404)
 
 
 
 
-@app.route('/delete_file', methods=['delete'])
+@application.route('/delete_file', methods=['delete'])
 def delete_file():
     job_id = request.args.get('job_id')
     if job_id is None:
@@ -60,22 +63,25 @@ def delete_file():
                            status_code=422, payload={'url': '/delete_file', 'method':'delete'})
     file_path = './airbnb homes scrape/home_' + job_id + '.json'
     if os.path.exists(file_path):
-        app.logger.info('file exists, will be removed')
+        application.logger.info('file exists, will be removed')
         os.remove(file_path)
-        app.logger.info('file removed')
+        application.logger.info('file removed')
         return 'File removed.'
     else:
-        app.logger.info('file does not exist')
+        application.logger.info('file does not exist')
         return 'file does not exist'
 
 
 
-@app.errorhandler(InvalidUsage)
+@application.errorhandler(InvalidUsage)
 def handle_invalid_usage(error):
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
     logging_dict = error.to_dict()
     logging_dict['status_code'] = error.status_code
-    app.logger.error(logging_dict)
+    application.logger.error(logging_dict)
     return response
 
+
+if __name__ == "__main__":
+    application.run()
---
+++ maintain_files.py
@@ -0,0 +1,18 @@
+import glob
+import os
+import time
+import threading
+
+EXPIRE_TIME = 10 * 60 ## Expire Time For JSON Files
+CLEAN_FILES_INTERVAL = 1 * 60  ## The interval of home json files cleaning task
+
+def clean_files_periodically(application):
+    application.logger.info('search all home json files')
+    json_file_paths = glob.glob('./airbnb homes scrape/home_*.json')
+    for path in json_file_paths:
+        if time.time() - os.path.getmtime(path) > EXPIRE_TIME:
+            os.remove(path)
+    application.logger.info('cleared expired home json files')
+    threading.Timer(CLEAN_FILES_INTERVAL, clean_files_periodically, [application]).start()
+            
+    
